e84939bf2b074f5d41d994c57cb2b319db360749
amalntk <amalntk07@gmail.com>
2022-04-25 09:01:02 +0530

    new

--- firstapp/urls.py
+++ firstapp/urls.py
@@ -3,14 +3,18 @@ from firstapp import views
 urlpatterns = [
     path('index',views.index,name='index'),
     path('login',views.login,name='login'),
-    # path('bmi.html',views.bmic,name='bmi'),
     path('reg',views.reg,name='register'),
     path('',views.welcome,name='welcome'),
     path('cipher.html',views.cipher,name='cipher'),
     path('welcome.html',views.logout,name='logout'),
     path('encrypt',views.encrypt,name='encrypt'),
     path('decrypt',views.decrypt,name='decrypt'),
-    path('articles',views.articles,name='articles')
+    path('articles',views.articles,name='articles'),
+    path('phising.html',views.phising,name='phising'),
+    path('predict',views.predict,name='predict'),
+    path('safe',views.safe,name='safe'),
+    path('unsafe',views.unsafe,name='unsafe')
+
 
 
 
--- firstapp/views.py
+++ firstapp/views.py
@@ -1,4 +1,8 @@
+from cmd import PROMPT
+from http.client import HTTPMessage
 from json import encoder
+from ssl import AlertDescription
+from django.http import HttpResponse
 from django.shortcuts import render,redirect
 # from django.contrib.auth.forms import UserCreationForm
 # from .forms import CreateUserForm
@@ -9,6 +13,7 @@ import os
 from .forms import encorder
 from django.contrib.auth.models import User
 from .forms import regg
+from firstapp.feature import generate_data_set 
 # Create your views here.
 SECURE=(('a','$'),('b','!'),('c','5'),('d','*'),('e','`'),('f','|'),('g','}'),('h','{'),('i',']'),('j','['),('k','/'),('l','?'),('m','.'),('n','~'),('o','!'),('p','@'),('q','#'),('r','$'),('s','%'),('t','^'),('u','&'),('v','-'),('w','_'),('x','+'),('y',')'),('z','('))
 def index(request):
@@ -58,16 +63,16 @@ def encrypt(request):
     else:
         form=encorder()
         return render(request,'encrypt.html',{'fm':form})
-
+    
 def decrypt(request):
     if request.method=="POST":
-        form=encorder(request.POST)
+        form = encorder(request.POST)
         if form.is_valid():
-            message =form.cleaned_data["text"]
+            message = form.cleaned_data["text"]
             for a,b in SECURE:
-                message=message.replace(b,a)
-                form=encorder()
-            return render(request,'decrypt.html',{'text':message, 'fm':form})
+                message = message .replace(b, a)
+                form = encorder()
+            return render(request,'decrypt.html',{'text':message,'fm':form})
     else:
         form=encorder()
         return render(request,'decrypt.html',{'fm':form}) 
@@ -75,3 +80,60 @@ def decrypt(request):
 def articles(request):
     return render(request,"articles.html")
 
+def phising(request):
+    return render(request,"phising.html")
+    # ggjgh
+    from flask import Flask, request, render_template
+import numpy as np
+import pandas as pd
+from sklearn import metrics 
+import warnings
+
+warnings.filterwarnings('ignore')
+
+# from feature import generate_data_set
+# Gradient Boosting Classifier Model
+from sklearn.ensemble import GradientBoostingClassifier
+
+data = pd.read_csv("phishing.csv")
+#droping index column
+data = data.drop(['Index'],axis = 1)
+# Splitting the dataset into dependant and independant fetature
+
+X = data.drop(["class"],axis =1)
+y = data["class"]
+
+# instantiate the model
+gbc = GradientBoostingClassifier(max_depth=4,learning_rate=0.7)
+
+# fit the model 
+gbc.fit(X,y)
+def predict(request):
+    if request.method == "POST":
+
+        # url = request.form["url"]
+        url=request.POST.get('url')
+        x = np.array(generate_data_set(url)).reshape(1,30) 
+        y_pred =gbc.predict(x)[0]
+        print(y_pred)
+        #1 is safe    
+        #-1 is unsafe   
+        y_pro_phishing = gbc.predict_proba(x)[0,0]
+        print(y_pro_phishing)
+        y_pro_non_phishing = gbc.predict_proba(x)[0,1]
+        print(y_pro_non_phishing)
+        if(y_pred ==1 ):
+            pred = "It is  safe to go ".format(y_pro_phishing*100)
+            return render(request,'safe.html',{'pred':pred})
+            
+        else:
+            pred = "It is unsafe to go ".format(y_pro_non_phishing*100)
+            return render(request,"unsafe.html",{'pred':pred})
+            # pred = "It is {0:.2f} % unsafe to go ".format(y_pro_non_phishing*100)
+            # return render_template('index.html',x =y_pro_non_phishing,url=url )
+    return render(request,"safe.html")
+
+def safe(request):
+    return render(request,"safe.html")
+def unsafe(request):
+    return render(request,"unsafe.html")    
